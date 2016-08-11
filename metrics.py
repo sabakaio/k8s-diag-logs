9859181b2485949bd0b71d3aab7934faf8138b8a
Anton Egorov <anton.egoroff@gmail.com>
2016-08-12 01:29:51 +0300

    configurable dsn

--- metrics.py
+++ metrics.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
+import os
 import itertools
+
 import requests
+from decouple import config, Csv
 from influxdb import InfluxDBClient
 
-client = InfluxDBClient.from_DSN('influxdb://localhost:8086/k8s', timeout=5)
-measurements = ['cpu/usage_rate', 'memory/usage']
 
-kubernetes_api = 'http://127.0.0.1:8001'
+kubernetes_api = config('K8S_API', 'http://localhost:8001')
+influxdb_dsn = config('INFLUXDB_DSN', 'influxdb://localhost:8086/k8s')
+measurements = config('MEASUREMENTS', 'cpu/usage_rate,memory/usage', cast=Csv())
+
+
+client = InfluxDBClient.from_DSN(influxdb_dsn, timeout=5)
+
 
 def k_get(k_type):
     url = kubernetes_api.strip('/') + '/api/v1/' + k_type.strip('/') + 's'
     res = requests.get(url)
     res.raise_for_status()
     return [item['metadata']['name'] for item in res.json()['items']]
+
 
 def metrics(k_type):
     for name in k_get(k_type):
@@ -26,6 +34,7 @@ def metrics(k_type):
             for r in itertools.chain(*res):
                 r.update(type=k_type, name=name)
                 yield r
+
 
 for r in itertools.chain(metrics('node'), metrics('pod')):
     print(r)
